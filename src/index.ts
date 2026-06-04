c5418acad010c25c36d89cad1760a1d09f96b958
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-06-04 02:59:31 +0200

    feat(index): add login to discord

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,10 @@ import { CrownutilsClient } from '@/client/crownutils-client.js';
 
 const client = new CrownutilsClient();
 await client.init();
+
+const token = process.env.DISCORD_TOKEN;
+if (!token) {
+  throw new Error('Missing DISCORD_TOKEN in environment.');
+}
+
+await client.login(token);
