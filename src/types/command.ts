05cd685f10a4d244bb6977cb55d2077319758403
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-06-06 03:54:51 +0200

    feat(types): Add CommandAuthorization & CommandScope #12

--- src/types/command.ts
+++ src/types/command.ts
@@ -5,8 +5,13 @@ import type {
   SlashCommandOptionsOnlyBuilder,
 } from 'discord.js';
 
+type CommandAuthorization = 'owner' | 'privileged' | 'public';
+type CommandScope = 'main_guild_only' | 'global';
+
 export interface SlashCommand {
   data: SlashCommandBuilder | SlashCommandOptionsOnlyBuilder;
+  authorization?: CommandAuthorization;
+  scope?: CommandScope;
   execute(interaction: ChatInputCommandInteraction): Promise<void>;
 }
 
@@ -14,5 +19,7 @@ export interface PrefixCommand {
   name: string;
   description: string;
   aliases?: string[];
+  authorization?: CommandAuthorization;
+  scope?: CommandScope;
   execute(interaction: Message, args: string[]): Promise<void>;
 }
