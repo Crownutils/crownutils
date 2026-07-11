9f5212a08d70721bffaca4c26edf9417b1add450
Ntalcme <57364571+Ntalcme@users.noreply.github.com>
2026-07-11 04:52:41 +0200

    Refacto: delete reminder model #110

--- prisma/migrations/20260709043638_update_reminders/migration.sql
+++
@@ -1,9 +0,0 @@
--- AlterTable
-ALTER TABLE "Reminder" ADD COLUMN "lastError" TEXT;
-ALTER TABLE "Reminder" ADD COLUMN "sentAt" DATETIME;
-
--- CreateIndex
-CREATE INDEX "Reminder_status_dueAt_idx" ON "Reminder"("status", "dueAt");
-
--- CreateIndex
-CREATE INDEX "Reminder_userId_idx" ON "Reminder"("userId");
